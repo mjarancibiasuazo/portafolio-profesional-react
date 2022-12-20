6b24bbf48b587601f7e408543ccf5ff6eb5e8f05
Jose Arancibia suazo <mjarancibiasuazo@gmail.com>
2022-12-20 16:28:12 -0300

    respaldo 20-12-2022

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { trabajos } from '../data/trabajos';
 
 export const Portafolio = () => {
   return (
     <div className='page'>
       <h1 className='heading'>Portafolio</h1>
+
+      {
+        trabajos.map(trabajo => {
+        return(
+          <article key={ trabajo.id }>
+            <div className="mask">
+              <img src={"/images/"+trabajo.id+".png"}/>
+
+            </div>
+
+            <span>{ trabajo.categorias }</span>
+            <h2><Link to={"/proyecto/"+trabajo.id}>{ trabajo.nombre }</Link></h2>
+            <h3>{ trabajo.tecnologias }</h3>
+
+          </article>
+        );
+      })
+     }
+
     </div>
   )
 }
---
+++ src/data/trabajos.js
@@ -0,0 +1,26 @@
+export const trabajos = [
+    {
+        'id': 'victorroblesweb',
+        'nombre': 'Victor Robles WEB',
+        'url': 'victorrobles.es',
+        'tecnologias': 'HTML, CSS, JS, PHP, MySQL',
+        'categorias': 'Desarrollo web',
+        'descripcion': 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+    },
+    {
+        'id': 'facebook',
+        'nombre': 'Facebook',
+        'url': 'facebook.com',
+        'tecnologias': 'React, Node, Mongo',
+        'categorias': 'Desarrollo web',
+        'descripcion': 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+    },
+    {
+        'id': 'twitter',
+        'nombre': 'Twitter',
+        'url': 'twitter.com',
+        'tecnologias': 'HTML, CSS',
+        'categorias': 'Maquetación web',
+        'descripcion': 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+    }
+]; 
